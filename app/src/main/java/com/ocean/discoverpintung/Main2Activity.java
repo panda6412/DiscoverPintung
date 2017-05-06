2677c7089bf1d1d38a3658becddd48e1aa86fbc8
puppy <twpsdance@gmail.com>
2017-05-06 23:04:50 +0800

    no message

---
+++ app/src/main/java/com/ocean/discoverpintung/Main2Activity.java
@@ -0,0 +1,43 @@
+package com.ocean.discoverpintung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        ListView list =(ListView)findViewById(R.id.list);
+
+
+  class adapter extends BaseAdapter {
+         @Override
+         public int getCount() {
+             return 0;
+         }
+
+         @Override
+         public Object getItem(int position) {
+
+             return null;
+         }
+
+         @Override
+         public long getItemId(int position) {
+             return 0;
+         }
+
+         @Override
+         public View getView(int position, View convertView, ViewGroup parent) {
+             return null;
+         }
+     }
+
+   }
+}
